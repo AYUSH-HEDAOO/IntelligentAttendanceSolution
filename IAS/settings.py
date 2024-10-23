9121cdadaa6eea3d0c037ea0a7243d6250bdec53
Praful Patekar <56212683+Prafulpatekar@users.noreply.github.com>
2024-10-24 00:18:56 +0530

    Update settings.py
    Adding IP address

--- IAS/settings.py
+++ IAS/settings.py
@@ -27,7 +27,7 @@ APP_DIR = BASE_DIR / "core_apps"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "13.233.153.39"]
 
 
 # Application definition
