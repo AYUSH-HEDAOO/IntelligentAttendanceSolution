eaae1c94c77a25c0b748a3416b42b82eced9c727
Praful Patekar <prafulpatekar23@gmail.com>
2025-03-24 00:33:35 +0530

    Adding Staff and Owner Profile

--- IAS/core_apps/institutes/models.py
+++ IAS/core_apps/institutes/models.py
@@ -5,6 +5,10 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 from ias.core_apps.common.models import IASModel
 
+def institute_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return f"{instance.id}/profile_images/{filename}"
+
 # Institute model
 class Institute(IASModel):
     institute_reg_number = models.CharField(max_length=100,null=True, blank=True)
@@ -12,7 +16,7 @@ class Institute(IASModel):
     phone_number = PhoneNumberField(verbose_name=_("phone number"), max_length=30, default="", region="IN")
     address = models.TextField(verbose_name=_("address"), default="")
     city = models.CharField(verbose_name=_("city"), max_length=180, default="Nagpur", blank=False, null=False)
-    institute_image = models.ImageField(verbose_name=_("institute image"), default="/profile_default.png")
+    institute_image = models.ImageField(verbose_name=_("institute image"), default="/profile_images/default_profile.png", upload_to=institute_directory_path)
 
     def __str__(self):
         return f"{self.institute_name}"
--- IAS/core_apps/staffs/models.py
+++ IAS/core_apps/staffs/models.py
@@ -3,6 +3,10 @@ from ias.core_apps.common.models import IASModel,BloodGroup, Gender
 from ias.core_apps.users.models import Role
 from ias.core_apps.institutes.models import Department, Designation,Institute
 
+def staff_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return f"{instance.institute.id}/{instance.id}/{filename}"
+
 # Create your models here.
 class Staff(IASModel):
     role = models.OneToOneField(Role, on_delete=models.CASCADE, related_name="role_staff")
@@ -10,7 +14,7 @@ class Staff(IASModel):
     designation = models.ForeignKey(Designation, on_delete=models.CASCADE, related_name="staff_designation", null=True)
     institute = models.ForeignKey(Institute, on_delete=models.CASCADE, related_name="institute_staff",null=True)
     created_by_uuid_role = models.TextField(null=True, blank=True)
-    profile_image = models.ImageField(blank=True, default="/profile_images/default_profile.png")
+    profile_image = models.ImageField(blank=True, default="/profile_images/default_profile.png", upload_to=staff_directory_path)
     dob = models.DateField(null=True, blank=True)
     state = models.CharField(max_length=30, null=True, blank=True)
     address = models.CharField(max_length=400, null=True, blank=True)
