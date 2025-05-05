abea2a4b7b62f7f9910d8f1218c4c86c171ac69a
Praful Patekar <prafulpatekar23@gmail.com>
2025-05-05 12:31:21 +0530

    Path Correction

--- IAS/scripts/train_face_recognization_model.py
+++ IAS/scripts/train_face_recognization_model.py
@@ -35,8 +35,8 @@ def delete_trained_images(training_dir: str) -> None:
 
 def start_training(institute) -> bool:
     logger.info(f"Training Started for {institute.institute_name}.")
-    # training_dir = os.path.join(MEDIA_ROOT, "image_dataset", str(institute.id))
-    training_dir = f"{MEDIA_ROOT}/image_dataset/{institute.id}"
+    training_dir = os.path.join(MEDIA_ROOT, "image_dataset", str(institute.id))
+    # training_dir = f"{MEDIA_ROOT}/image_dataset/{institute.id}"
     if not os.path.exists(training_dir):
         logger.info(f"Training Directory not found for {institute.institute_name}")
         logger.info("Please upload images from any Student, Staff and Institute Login.\n")
@@ -74,8 +74,8 @@ def start_training(institute) -> bool:
     y = encoder.transform(y)
     X1 = np.array(X)
 
-    # trained_data_path = os.path.join(BASR_DIR, "IAS", "face_recognition_data")
-    trained_data_path = f"{BASR_DIR}/IAS/face_recognition_data"
+    trained_data_path = os.path.join(BASR_DIR, "IAS", "face_recognition_data")
+    # trained_data_path = f"{BASR_DIR}/IAS/face_recognition_data"
     if not os.path.exists(trained_data_path):
         os.mkdir(trained_data_path)
     classes_path = os.path.join(trained_data_path, "classes.npy")
